d49cde5b5b5d87725317b2f081e43e794b01bd35
Ibad Ahmed <ibadahmed353@gmail.com>
2025-10-27 16:02:31 +0500

    just checking

--- Lab1.java
+++ Lab1.java
@@ -1,5 +1,6 @@
 public class Lab1{
     public static void main(String[] args) {
-        System.out.println("Name:Ibad Ahmed Siddiqui\nRoll no.:2023F-BSE-128\nCourse:Software Constructon and Development");
+        System.out.println("Hello World");
     }
+
 }
