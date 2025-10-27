c1a95d3cc8203f1815c2c546df87bbd51c5bb21e
Ibad Ahmed <ibadahmed353@gmail.com>
2025-10-27 15:48:56 +0500

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,5 @@
+public class Lab1{
+    public static void main(String[] args) {
+        System.out.println("Name:Ibad Ahmed Siddiqui\nRoll no.:2023F-BSE-128\nCourse:Software Constructon and Development");
+    }
+}
